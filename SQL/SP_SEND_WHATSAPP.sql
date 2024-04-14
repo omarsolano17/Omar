ba2fcf65d38c257a9deab2d7b0dfb899e63bdc32
omarsolano17 <84154274+omarsolano17@users.noreply.github.com>
2024-04-14 10:27:36 -0500

    Avances page whatsapp

--- SQL/SP_SEND_WHATSAPP.sql
+++ SQL/SP_SEND_WHATSAPP.sql
@@ -3,12 +3,14 @@ CREATE OR ALTER PROCEDURE DBO.SP_SEND_WHATSAPP
 (
 	@NUMERO VARCHAR(13),
 	@MENSAJE VARCHAR(max),
+	@IDAPI SMALLINT=NULL,
 	@ENDPOINT VARCHAR(100) = 'sendMessage', -- sendFile, sendPTT, sendLink, sendContact, sendLocation, sendVCard, forwardMessage, 
 	@NOMBREARCHIVO VARCHAR(100) = 'Archivo.pdf', 
 	@LINK VARCHAR(1000) = NULL,
-  @PROCEDENCIA VARCHAR(100) = NULL,
-  @API_WHATSAPP VARCHAR(256) = NULL
+  	@PROCEDENCIA VARCHAR(100) = NULL,
+  	@API_WHATSAPP VARCHAR(256) = NULL
 )
+--A
 WITH ENCRYPTION
 AS
 DECLARE @VARIABLE VARCHAR(20)
@@ -20,101 +22,73 @@ DECLARE @src VARCHAR(255)
 DECLARE @desc VARCHAR(255)
 DECLARE @Body VARCHAR(max) 
 BEGIN
-   -------------
+	-------------
 	IF DBO.FNK_VALORVARIABLE('WHATSAPP_HABILITADO')<>'SI' 
 	BEGIN
 		PRINT 'Envíos por WhatsApp deshabilitado (USVGS => WHATSAPP_HABILITADO)'
-		SELECT 'KO' OK, ERROR='Envíos por WhatsApp deshabilitado (USVGS => WHATSAPP_HABILITADO)'
+		RAISERROR('Envíos por WhatsApp deshabilitado (USVGS => WHATSAPP_HABILITADO)',16,1) 
 		RETURN
 	END
   
 	IF (SELECT COUNT(*) FROM TGEN WHERE TABLA='General' AND CAMPO='EXCLUYE_CELULAR' AND CODIGO=@NUMERO)>0 
 	BEGIN
-		PRINT 'NUMERO EXCLUIDO'
+		RAISERROR('NUMERO EXCLUIDO',16,1) 
 		RETURN
 	END
-  IF COALESCE(@API_WHATSAPP,'')=''
-	  SELECT @sUrl = OBSERVACION FROM USVGS WHERE IDVARIABLE = 'URL_API_WHATSAPP'
-  ELSE
-    SELECT @sUrl = @API_WHATSAPP
-
+	PRINT '@IDAPI='
+	PRINT @NUMERO
+	PRINT @MENSAJE
+	PRINT @IDAPI
+	IF COALESCE(@IDAPI,0)<>0
+	BEGIN
+		SELECT @sUrl = [ENDPOINT] FROM APIS WHERE ID=@IDAPI
+	END
+	ELSE
+	BEGIN
+		IF COALESCE(@API_WHATSAPP,'')=''
+			SELECT @sUrl = OBSERVACION FROM USVGS WHERE IDVARIABLE = 'URL_API_WHATSAPP'
+		ELSE
+			SELECT @sUrl = @API_WHATSAPP
+	END
+  SELECT  OBSERVACION FROM USVGS WHERE IDVARIABLE = 'URL_API_WHATSAPP'
 	IF ISNULL(@sUrl,'')=''
 	BEGIN
-		  RAISERROR('VARIABLE DE SISTEMA (URL_API_WHATSAPP) NO CONFIGURADA',16,1) 
-		  RETURN
+		RAISERROR('VARIABLE DE SISTEMA (URL_API_WHATSAPP) NO CONFIGURADA',16,1) 
+		RETURN
 	END
-	
+	PRINT @SURL
 	IF ISNULL(@MENSAJE,'')=''
 	BEGIN
-      PRINT 'MENSAJE VACIO'
-		  RETURN
+		RAISERROR('Mensaje Vacio',16,1) 
+		RETURN
 	END
 		
 	IF LEN(@NUMERO)<=8
 	BEGIN
-		PRINT 'LONGITUD DEL NUMERO <= 8'
+		RAISERROR('LONGITUD DEL NUMERO <= 8',16,1) 
 		RETURN
 	END
-  IF 1=2
-  BEGIN
-	  SELECT @sUrl = REPLACE(@sUrl,'ENDPOINT',COALESCE(@ENDPOINT,'sendMessage'))
-	  EXEC sys.sp_OACreate 'MSXML2.ServerXMLHttp', @obj OUT
-	  IF DBO.FNK_VALORVARIABLE('IXCOUNTRY') = 'PERU'
-	  BEGIN
-		  SET @Body ='{"body": "'+@MENSAJE+'","phone": '+RIGHT('51'+@NUMERO,11)+'}' 
-		  IF @ENDPOINT = 'sendFile' SET @Body ='{"body": "'+@MENSAJE+'", "phone": '+RIGHT('51'+@NUMERO,11)+', "filename": "'+@NOMBREARCHIVO+'"}' 
-	  END
-	  ELSE
-	  BEGIN
-		  SET @Body ='{"body": "'+@MENSAJE+'","phone": '+RIGHT('57'+@NUMERO,12)+'}' 
-		  IF @ENDPOINT = 'sendFile' SET @Body ='{"body": "'+@MENSAJE+'", "phone": '+RIGHT('51'+@NUMERO,12)+', "filename": "'+@NOMBREARCHIVO+'"}' 
-	  END
-	  EXEC sys.sp_OAMethod @obj, 'Open', NULL, 'POST', @sUrl, false
-	  EXEC sys.sp_OAMethod @Obj, 'setRequestHeader', null, 'Content-Type', 'application/json'
-	  EXEC SYS.sp_OAMethod @obj, 'send', null, @Body
-	  EXEC sys.sp_OAMethod @obj, 'responseText', @response OUTPUT
-
-	  IF @response IS NULL
-	  BEGIN
-		  DECLARE @TABLA_TMP1 AS TABLE (ITEM INT IDENTITY(1,1), RESPONSE VARCHAR(MAX))
-		  INSERT INTO @TABLA_TMP1(RESPONSE)
-		  EXEC sys.sp_OAGetProperty @obj, 'responseText'
-		  SELECT @response=RESPONSE FROM @TABLA_TMP1
-	  END
-
-	  EXEC sys.sp_OADestroy @obj
-
-     IF DBO.FNK_VALORVARIABLE('IXCOUNTRY') = 'PERU'
-     BEGIN
-        INSERT INTO SMSL(VIA,CONTACTO,MENSAJE,METHOD,RESPONSE,VARIABLE_API)
-	     SELECT 'WHATSAPP',@NUMERO,@MENSAJE,'POST', @response [response],@VARIABLE
-     END
-  END
-  ELSE
-  BEGIN
-    SELECT @sUrl = REPLACE(@sUrl,'ENDPOINT',COALESCE(@ENDPOINT,'sendMessage'))
-	  EXEC sys.sp_OACreate 'MSXML2.ServerXMLHttp', @obj OUT
+	BEGIN
+		--SELECT @sUrl = REPLACE(@sUrl,'ENDPOINT',COALESCE(@ENDPOINT,'sendMessage'))
+		EXEC sys.sp_OACreate 'MSXML2.ServerXMLHttp', @obj OUT
 		SET @Body ='{"message":"'+@MENSAJE+'", "phone":"'+RIGHT('57'+@NUMERO,12)+'"}' 
-	  EXEC sys.sp_OAMethod @obj, 'Open', NULL, 'POST', @sUrl, false
-	  EXEC sys.sp_OAMethod @Obj, 'setRequestHeader', null, 'Content-Type', 'application/json'
-	  EXEC SYS.sp_OAMethod @obj, 'send', null, @Body
-	  EXEC sys.sp_OAMethod @obj, 'responseText', @response OUTPUT
+		EXEC sys.sp_OAMethod @obj, 'Open', NULL, 'POST', @sUrl, false
+		EXEC sys.sp_OAMethod @Obj, 'setRequestHeader', null, 'Content-Type', 'application/json'
+		EXEC SYS.sp_OAMethod @obj, 'send', null, @Body
+		EXEC sys.sp_OAMethod @obj, 'responseText', @response OUTPUT
 
-	  IF @response IS NULL
-	  BEGIN
-		  DECLARE @TABLA_TMP AS TABLE (ITEM INT IDENTITY(1,1), RESPONSE VARCHAR(MAX))
-		  INSERT INTO @TABLA_TMP(RESPONSE)
-		  EXEC sys.sp_OAGetProperty @obj, 'responseText'
-		  SELECT @response=RESPONSE FROM @TABLA_TMP
-	  END
+		IF @response IS NULL
+		BEGIN
+			DECLARE @TABLA_TMP AS TABLE (ITEM INT IDENTITY(1,1), RESPONSE VARCHAR(MAX))
+			INSERT INTO @TABLA_TMP(RESPONSE)
+			EXEC sys.sp_OAGetProperty @obj, 'responseText'
+			SELECT @response=RESPONSE FROM @TABLA_TMP
+		END
 
-    EXEC sys.sp_OADestroy @obj
-    BEGIN
-      INSERT INTO SMSL(VIA,CONTACTO,MENSAJE,METHOD,RESPONSE,VARIABLE_API)
-      SELECT 'WHATSAPP',@NUMERO,@MENSAJE,'POST', @response [response],RIGHT(@sUrl,20)
-    END
-  END
-END 
---SELECT * FROM NOTI ORDER BY 1 DESC
---SELECT * FROM SMSL ORDER BY 1 DESC
-
+		EXEC sys.sp_OADestroy @obj
+		BEGIN
+			INSERT INTO SMSL(VIA,CONTACTO,MENSAJE,METHOD,RESPONSE,VARIABLE_API)
+			SELECT 'WHATSAPP',@NUMERO,@MENSAJE,'POST', @response [response],@sUrl
+		END    
+	END
+END
--- src/pages/Mensajes/IndexPage.vue
+++ src/pages/Mensajes/IndexPage.vue
@@ -21,7 +21,7 @@
       <div class="col-12" v-if="vista==='sms'">
         <q-bar style="min-width: 250px;" class="bg-teal text-white rounded-borders">
           <div class="cursor-pointer non-selectable">
-            File
+            Menu
             <q-menu fit >
               <q-list style="min-width: 100px" dense separator>
                 <q-item clickable v-close-popup dense @click="vista2='Mensajes'">
@@ -48,6 +48,22 @@
                     </q-item-label>
                   </q-item-section>
                 </q-item>
+                <q-item clickable v-close-popup dense @click="vista2='Contactos'">
+                  <q-item-section avatar >
+                    <q-item-label class="row flex flex-center">
+                      <q-avatar color="green" text-color="white" icon="message" dense size="sm" class="q-mr-xs"/>
+                      Contactos
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item clickable v-close-popup dense @click="vista2='Historial'">
+                  <q-item-section avatar >
+                    <q-item-label class="row flex flex-center">
+                      <q-avatar color="green" text-color="white" icon="message" dense size="sm" class="q-mr-xs"/>
+                      Historial
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
               </q-list>
             </q-menu>
           </div>
@@ -68,32 +84,159 @@
         </q-bar>
         <div class="row col-12 flex q-pt-xs" v-if="vista2==='Pruebas'">
           <q-btn class="row col-12 q-ml-sm" round dense color="green-4" label="Enviar"  icon="send" @click="onSMS()" flat/>
+          <q-select class="q-pa-xs col-3" v-model="data.IDAPI" outlined dense emit-value map-options :options="apis" clr label="Enviar Desde"
+            option-value="ID" :option-label="'DESCRIPCION'"/>
+          <q-input class="q-pa-xs col-2" v-model="data.NUMERO" outlined dense type="number" label="Enviar a:" :maxlength="10"
+          :rules="[(val) => (+val?.length === 10 && val?.toString()?.substring(0, 1) == '3') || 'Numero de celular incorrecto',]"/>
+          <q-input class="q-pa-xs col-7" v-model="data.MENSAJE" outlined dense type="textarea" autogrow label="Mensaje" />
+        </div>
+        <div class="row col-12 flex q-pt-xs" v-if="vista2==='Historial'">
+          <q-select class="q-pa-xs col-3" v-model="filtro.MEDIO" outlined dense emit-value map-options clearable
+            :options="[{value: 'SMS', label:'Texto'},{value: 'WHATSAPP', label:'Whatsapp'},]"  label="Medio"/>
+          <q-select v-if="filtro.MEDIO==='WHATSAPP'" class="q-pa-xs col-3" v-model="filtro.IDAPI" outlined dense emit-value map-options clearable 
+            :options="apis" clr label="Enviados Desde:" option-value="ID" :option-label="'DESCRIPCION'"/>
+            <!-- {{ whereSMSL }} -->
+          <ServerSideTableComponent
+            ref="tableSMSLref"
+            titulo="Mensajes enviados"
+            tabla="SMSL"
+            :columnas_visibles="dtSMSL.columnas_visibles"
+            :columnas="dtSMSL.columnas"
+            :columnas_filtro="dtSMSL.columnas_filtro"
+            row_key=" ITEM "
+            ordenarPor="ITEM DESC"
+            :dense="true"
+            colorSelect="green-2"
+            class="col-12"
+            flat
+            :rows_per_page_options="[5, 10, 25, 50, 100]"
+            :rowsPerPage="10"
+            :condicion_adicional="whereSMSL"
+            />
+            <!-- :left_join="dtUSUSU.left_join" -->
+            <!-- :permiteExportar="false" -->
+            <!-- :permisoColumnas="false" -->
+            <!-- :inner_join="dtUSUSU.inner_join" -->
+            <!-- :columnas_visibles="dtUSUSU.columnas_visibles" -->
+            <!-- @fila="onSelectUsusu($event)" -->
+            <!-- @evento_generico="onEventoGenerico($event)" -->
+        </div>
+        <div class="row col-12 flex q-pt-xs" v-if="vista2==='Contactos'">
+          <q-select class="q-pa-xs col-3" v-model="filtro.IDAPI" outlined dense emit-value map-options clearable 
+            :options="apis" clr label="Cargar desde:" option-value="ID" :option-label="'DESCRIPCION'"/>
+          <!-- <ServerSideTableComponent
+            ref="tableSMSLref"
+            titulo="Mensajes enviados"
+            tabla="SMSL"
+            :columnas_visibles="dtSMSL.columnas_visibles"
+            :columnas="dtSMSL.columnas"
+            :columnas_filtro="dtSMSL.columnas_filtro"
+            row_key=" ITEM "
+            ordenarPor="ITEM DESC"
+            :dense="true"
+            colorSelect="green-2"
+            class="col-12"
+            flat
+            :rows_per_page_options="[5, 10, 25, 50, 100]"
+            :rowsPerPage="10"
+            :condicion_adicional="whereSMSL"
+            /> -->
+        </div>
+      </div>
+      <div class="col-12" v-else>        
+        <q-btn class="row col-12 q-ml-sm" round dense color="green-4" label="Agregar"  icon="add" @click="onAPIS('NUEVO')" flat v-if="usuario?.USUARIO==='OSOLANO'"/>
+        <q-markup-table class="tabla2"  dense v-if="apis?.length>0">
+          <thead>
+            <tr>
+              <th class="text-center">Identificador</th>
+              <th class="text-left">Sede</th>
+              <th class="text-left">EndPoint</th>
+              <th class="text-left">Numero</th>
+              <th class="text-left">Descripcion</th>
+            </tr>
+          </thead>
+          <tbody>
+            <template v-for="(x,i) in apis" :key="i">
+              <tr @click="apiSelected = x, onAPIS('UPDATE',x)" class="cursor-pointer" :class="x==apiSelected ? 'bg-secondary':''" >
+                <td class="text-center">{{x?.ID}}</td>
+                <td class="text-left">{{x?.IDSEDE}} {{ x?.DESCRIPCION_SEDE }}</td>
+                <td class="text-left">{{x?.ENDPOINT}}</td>
+                <td class="text-left">{{x?.NUMERO}}</td>
+                <td class="text-left">{{x?.DESCRIPCION}}</td>
+              </tr>
+            </template>
+          </tbody>
+        </q-markup-table>
+        <div class="row col-12 flex q-pt-xs" v-if="false">
           <q-input class="q-pa-xs col-3" v-model="data.NUMERO" outlined dense type="number" label="Celular" :maxlength="10"
           :rules="[(val) => (+val?.length === 10 && val?.toString()?.substring(0, 1) == '3') || 'Numero de celular incorrecto',]"/>
           <q-input class="q-pa-xs col-9" v-model="data.MENSAJE" outlined dense type="textarea" autogrow label="Mensaje" />
         </div>
       </div>
-      <div class="col-12" v-else>
-        settigns
-      </div>
     </div>
+    
+    <q-dialog v-model="api.DGAPI" persistent>
+      <q-card style="width: 70%; max-width: 95%">
+        <q-bar class="text-white bg-primary">
+          <!-- <span>{{ ususu?.METODO.charAt(0).toUpperCase() + ususu?.METODO.slice(1).toLowerCase() }} Usuario: {{ ususu?.USUARIO }} - {{ ususu?.NOMBRE }}</span> -->
+          <q-space />
+
+          <q-btn flat icon="close" v-close-popup />
+        </q-bar>
+        <q-card-section class="row items-center">
+          <div class="row col-12">
+            <!-- IDSEDE, [ENDPOINT], NUMERO, DESCRIPCION, ACTIVO -->
+            <q-input  class="q-pa-xs col-1" v-model="api.ID" type="text" outlined dense label="Id" disable/>
+            <q-input  class="q-pa-xs col-5" v-model="api.ENDPOINT" type="text" outlined dense label="EndPoint" v-show="api.METODO === 'INSERTAR'"/>
+            <q-select class="q-pa-xs col-6" v-model="api.IDSEDE" outlined dense emit-value map-options options="opt.sede" clr label="Sede"/>
+            <q-input  class="q-pa-xs col-3" v-model="api.NUMERO" outlined dense type="number" label="Celular / Numero" :maxlength="10"
+            :rules="[(val) => (+val?.length === 10 && val?.toString()?.substring(0, 1) == '3') || 'Numero de celular incorrecto',]"/>
+            <q-input  class="q-pa-xs col-7" v-model="api.DESCRIPCION" type="text" outlined dense label="Denominacion / Descripcion"/>
+            <q-toggle v-model="api.ACTIVO" color="red" label="Activo?"/>
+          </div>
+        </q-card-section>
+        <q-card-actions align="right">
+          <q-btn flat label="Cancelar" color="secondary" v-close-popup />
+          <q-btn flat :label="api?.METODO?.toLowerCase()" color="primary" @click="onAPIS(api?.METODO)"/>
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
   </q-page>
 </template>
 
 <script setup>
 import { useQuasar } from "quasar";
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, watch } from "vue";
 import { useAppStore } from "stores/app";
 import { useSeguridadStore } from "stores/seguridad";
+import ServerSideTableComponent from "src/components/ServerSideTableComponent.vue";
+
 //#region DATA
 const $q = useQuasar();
 const appStore = useAppStore();
 const authStore = useSeguridadStore();
+const filtro = ref({});
 const filter = ref("");
 const vista = ref("sms");
-const vista2 = ref("Pruebas");
+const vista2 = ref("Contactos");
+
+const api = ref({});
+const apiSelected = ref({});
+const apis = ref([]);
+const apiFrom = ref(null);
+const tableSMSLref = ref(null);
 const data = ref({});
-const buscarRef = ref(null);
+const dtSMSL = ref({
+  columnas: [
+    {name: "VIA", required: true, label: "Medio", align: "left", field: (row) => row.VIA, format: (val) => val, sortable: true},
+    {name: "CONTACTO", required: true, label: "Numero", align: "left", field: (row) => row.CONTACTO, format: (val) => val, sortable: true},
+    {name: "MENSAJE", required: true, label: "Mensaje", align: "left", field: (row) => row.MENSAJE, format: (val) => val, sortable: true},
+  ],
+  columnas_visibles: "ITEM, VIA, CONTACTO, MENSAJE",
+  columnas_filtro: "CONTACTO, MENSAJE"
+})
+
+
 //#endregion
 
 //#region METHODS
@@ -102,33 +245,87 @@ const onSMS = () => {
   .json({
     MODELO: "SMS",
     METODO: "ENVIAR",
-    PARAMETROS: { NUMERO:  data?.value?.NUMERO, MENSAJE: data?.value?.MENSAJE,},
+    PARAMETROS: { NUMERO:  data?.value?.NUMERO, MENSAJE: data?.value?.MENSAJE, IDAPI: data?.value?.IDAPI},
   })
   .then((res) => {
     if (res?.data?.result?.recordsets[0][0]?.OK === "OK") {
+      apiFrom.value = data?.value?.IDAPI
       $q.notify({message: `Mensaje de prueba enviado a ${data?.value?.NUMERO} Correctamente`,  color: 'positive', position: 'top', timeout: 2000 })
       data.value = {}
+      data.value.IDAPI = apiFrom.value
     }
   });
+};
+const onAPIS = (metodo, fila) => {
+  if (metodo === "NUEVO") {
+    api.value = {};
+    api.value.METODO = "INSERTAR";
+    api.value.IDSEDE = usuario?.value?.IDSEDE;
+    api.value.ENDPOINT = 'http://';
+    api.value.ACTIVO = true;
+    api.value.DGAPI = true;
+  } else if (metodo === "UPDATE") {
+    api.value = { ...fila };
+    api.value.METODO = "EDITAR";
+    api.value.DGAPI = true;
+  } else if (metodo === "INSERTAR" || metodo === "EDITAR" || metodo==='CONSULTAR') {
+    appStore
+      .json({
+        MODELO: "APIS",
+        METODO: metodo,
+        PARAMETROS: {
+          ID:         api?.value?.ID,
+          IDSEDE:     api?.value?.IDSEDE,
+          ENDPOINT:   api?.value?.ENDPOINT,
+          ACTIVO:     api?.value?.ACTIVO,
+          IDSEDE:     api?.value?.IDSEDE,
+          NUMERO:     api?.value?.NUMERO,
+          DESCRIPCION:api?.value?.DESCRIPCION
+        }
+      })
+      .then((res) => {
+        if (res?.data?.result?.recordsets[0][0]?.OK === "OK") {
+          apis.value = res?.data?.result?.recordsets[1]
+          if (!data?.value?.IDAPI) apiFrom.value = apis?.value[0]?.ID
+          if (metodo==='CONSULTAR') return
+
+          $q.notify({message: `Api Procesado de manera correcta`,  color: 'positive', position: 'top', timeout: 2000 })
+          api.value = {};
+          api.value.DGAPI = false;
+        }
+      });
+  }
 };
 //#endregion
 
 //#region COMPUTED
-// const menus = computed(() => {
-//   let menus = appStore.getMenuConfiguracion;
-//   if (!menus) return [];
-//   if (filter.value && filter.value !== "") {
-//     menus = menus.filter(
-//       (x) =>
-//         x.title.toLowerCase().indexOf(filter.value.toLowerCase()) !== -1 ||
-//         x.caption.toLowerCase().indexOf(filter.value.toLowerCase()) !== -1
-//     );
-//   }
-//   return menus;
-// });
+const usuario = computed(() => {
+  return authStore?.getUsuario;
+});
+const whereSMSL = computed(() => {
+  let r = ''
+  if (filtro?.value?.MEDIO){
+    r = ` VIA='${filtro?.value?.MEDIO}'`
+    if (filtro?.value?.MEDIO==='WHATSAPP' && filtro?.value?.IDAPI){
+      r += ` AND EXISTS(SELECT 1 FROM APIS A WHERE A.ID='${filtro?.value?.IDAPI}' AND A.[ENDPOINT]=VARIABLE_API)`
+    }
+  }
+  return r
+});
+//#endregion
+
+//#region Watch
+watch(whereSMSL, (newWhere, oldWhere) => {
+  if (vista2?.value==='Historial'){
+    tableSMSLref.value.filter=null
+    setTimeout(() => {
+      tableSMSLref.value.obligarRefrescar()
+    }, 100);
+  }
+});
 //#endregion
 onMounted(() => {
-  data.value.NUMERO = authStore?.getUsuario?.CELULAR
-  
+  onAPIS('CONSULTAR')
+  data.value.NUMERO = usuario?.value?.CELULAR
 });
 </script>
